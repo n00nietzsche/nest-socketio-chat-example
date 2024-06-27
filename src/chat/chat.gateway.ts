2b1b0e8eec4df64b74d7eef38f1fd74f7b1600a0
jakeseo <n00nietzsche@gmail.com>
2024-06-27 23:13:48 +0900

    feat: 기본 웹소켓 메세지 핸들러 추가

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { ChatService } from './chat.service';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  // ws://localhost:3000 로 연결 후 `message` 이벤트를 전송하면 payload 가 들어옴
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: string): void {
+    console.log('payload', payload);
+    this.server.emit('message', payload);
+  }
 }
