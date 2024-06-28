bb9171ae0063fb331a7f3f9ed76bb4d6c758e80e
jakeseo <n00nietzsche@gmail.com>
2024-06-28 16:55:26 +0900

    feat, test: 방 접속 및 방과 관련된 로직 테스트 코드 추가

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,22 +1,62 @@
 import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private readonly chatService: ChatService) {}
 
   @WebSocketServer()
   server: Server;
 
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
   // ws://localhost:3000 로 연결 후 `message` 이벤트를 전송하면 payload 가 들어옴
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: string): void {
-    console.log('payload', payload);
-    this.server.emit('message', payload);
+  handleMessage(
+    client: Socket,
+    payload: { room: string; message: string },
+  ): void {
+    console.log('Received payload:', payload);
+
+    this.server.to(payload.room).emit('message', {
+      room: payload.room,
+      message: payload.message,
+      sender: client.id,
+    });
+
+    console.log(`Message sent to room ${payload.room}`);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, payload: { room: string }): void {
+    const { room } = payload;
+
+    client.join(room);
+    client.emit('joinRoom', room);
+
+    console.log(`Client ${client.id} joined room: ${room}`);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleLeaveRoom(client: Socket, payload: { room: string }): void {
+    const { room } = payload;
+
+    client.leave(room);
+    client.emit('leaveRoom', room);
+
+    console.log(`Client ${client.id} left room: ${room}`);
   }
 }
--- test/chat/chat.e2e.test.ts
+++ test/chat/chat.e2e.test.ts
@@ -7,7 +7,7 @@ describe('ChatGateway (e2e)', () => {
   let app: INestApplication;
   let socket: Socket;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -20,18 +20,87 @@ describe('ChatGateway (e2e)', () => {
     socket = io('http://localhost:3000');
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     socket.disconnect();
     await app.close();
   });
 
-  it('message 이벤트에 내용을 전달하면, payload 를 클라이언트에게 브로드캐스팅한다.', (done) => {
-    socket.emit('message', 'Hello Server');
-
-    socket.on('message', (data) => {
-      console.log('data', data);
-      expect(data).toBe('Hello Server');
-      done();
+  it('room1 에 참여하면, room1 에 해당하는 메세지만 전달받는다.', (done) => {
+    // given: room1 에 참여하면
+    socket.emit('joinRoom', {
+      room: 'room1',
     });
+
+    // when: room1 과 room2 에 메세지가 전송됐을 때
+    socket.emit('message', {
+      room: 'room1',
+      message: 'test message1',
+    });
+
+    socket.emit('message', {
+      room: 'room2',
+      message: 'test message2',
+    });
+
+    // then: room1 로 전송된 메세지를 정상적으로 받을 수 있다.
+    socket.on(
+      'message',
+      (data: { room: string; message: string; sender: string }) => {
+        expect(data.room).toBe('room1');
+        expect(data.message).toBe('test message1');
+        expect(data.sender).toBeDefined();
+        done();
+      },
+    );
+  });
+
+  it('room2 에 참여하면, room2 에 해당하는 메세지만 전달받는다.', (done) => {
+    // given: room2 에 참여하면
+    socket.emit('joinRoom', {
+      room: 'room2',
+    });
+
+    // when: room2 에 메세지가 전송됐을 때
+    socket.emit('message', {
+      room: 'room2',
+      message: 'test message2',
+    });
+
+    // then: room2 로 전송된 메세지를 정상적으로 받을 수 있다.
+    socket.on(
+      'message',
+      (data: { room: string; message: string; sender: string }) => {
+        expect(data.room).toBe('room2');
+        expect(data.message).toBe('test message2');
+        expect(data.sender).toBeDefined();
+        done();
+      },
+    );
+  });
+
+  it('room 을 떠나면, 메세지를 전달받지 않는다.', (done) => {
+    // given: room2 에 참여한 뒤 room 을 떠나면,
+    socket.emit('joinRoom', {
+      room: 'room2',
+    });
+
+    socket.emit('leaveRoom', {
+      room: 'room2',
+    });
+
+    // when: room2 에 메세지가 전송됐을 때
+    socket.emit('message', {
+      room: 'room2',
+      message: 'test message2',
+    });
+
+    // then: room2 로 전송된 메세지를 정상적으로 받을 수 있다.
+    socket.on('message', () => {
+      done(new Error('방을 나갔기 때문에 메세지를 받아서는 안됩니다.'));
+    });
+
+    setTimeout(() => {
+      done();
+    }, 500);
   });
 });
