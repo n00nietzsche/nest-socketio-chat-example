3b000c893d5ca71d948d51369f7a726cff46c08d
jakeseo <n00nietzsche@gmail.com>
2024-07-01 22:50:34 +0900

    feat: 클라이언트 확인용 index.html 과 client.js 파일 추가 및 /index.html 에서 렌더링

---
+++ public/client.js
@@ -0,0 +1,29 @@
+const socket = io();
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const messages = document.getElementById('messages');
+
+socket.on('connect', () => {
+  socket.emit('joinRoom', { room: 'room1' });
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input.value) {
+    socket.emit('message', {
+      room: 'room1',
+      message: input.value,
+      sender: 'client',
+    });
+
+    input.value = '';
+  }
+});
+
+socket.on('message', (message) => {
+  const item = document.createElement('li');
+  item.textContent = message;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    ChatModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
